5934fdb59a3e42e67ef2947ba7f665d55263dd75
Braden Bird <15621005+bradenbird@users.noreply.github.com>
2023-12-03 13:44:08 -0600

    Format with Ruff

--- day1/day1.py
+++ day1/day1.py
@@ -1,17 +1,18 @@
 from argparse import ArgumentParser
 
 DIGIT_STRS = {
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9,
-    'zero': 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "zero": 0,
 }
+
 
 def get_left_number(line: str, digit_only: bool):
     length = len(line)
@@ -27,6 +28,7 @@ def get_left_number(line: str, digit_only: bool):
     # No matches, return 0
     return 0
 
+
 def get_right_number(line: str, digit_only: bool):
     end_index = len(line) - 1
     while end_index >= 0:
@@ -34,60 +36,64 @@ def get_right_number(line: str, digit_only: bool):
             return int(line[end_index])
         if not digit_only:
             for s, num in DIGIT_STRS.items():
-                if line[:end_index+1].endswith(s):
+                if line[: end_index + 1].endswith(s):
                     return num
         end_index -= 1
     # No matches, return 0
     return 0
 
+
 def number_from_line(line: str, digit_only: bool) -> int:
     return get_left_number(line, digit_only) * 10 + get_right_number(line, digit_only)
 
+
 def solution(data: str, digit_only: bool) -> int:
     total = 0
-    for line in data.split('\n'):
+    for line in data.split("\n"):
         total += number_from_line(line, digit_only)
     return total
+
 
 def main():
     parser = ArgumentParser()
     parser.add_argument(
         "-f",
-        '--filename',
+        "--filename",
         default=None,
-        help='Filename containing input data. If not provided, uses the example from the problem',
+        help="Filename containing input data. If not provided, uses the example from the problem",
     )
 
     parser.add_argument(
-        '--digit-only',
+        "--digit-only",
         default=False,
-        action='store_true',
-        help='Use the part-1 line parsing on the input'
+        action="store_true",
+        help="Use the part-1 line parsing on the input",
     )
 
     args = parser.parse_args()
 
-# Part 1 example data, can use instead if running part 1
-#     data = '''1abc2
-# pqr3stu8vwx
-# a1b2c3d4e5f
-# treb7uchet'''
+    # Part 1 example data, can use instead if running part 1
+    #     data = '''1abc2
+    # pqr3stu8vwx
+    # a1b2c3d4e5f
+    # treb7uchet'''
 
-    data = '''two1nine
+    data = """two1nine
 eightwothree
 abcone2threexyz
 xtwone3four
 4nineeightseven2
 zoneight234
-7pqrstsixteen'''
+7pqrstsixteen"""
 
     if args.filename:
         with open(args.filename) as f:
             data = f.read()
-    
+
     output = solution(data, args.digit_only)
-    
-    print(f'Solution: {output}')
+
+    print(f"Solution: {output}")
+
 
 if __name__ == "__main__":
     main()
--- day2/day2.py
+++ day2/day2.py
@@ -3,37 +3,43 @@ from collections import defaultdict
 import math
 import re
 
-LIMITS = defaultdict(int, {
-    'red': 12,
-    'green': 13,
-    'blue': 14,
-})
+LIMITS = defaultdict(
+    int,
+    {
+        "red": 12,
+        "green": 13,
+        "blue": 14,
+    },
+)
 
 # compile all patterns only once
-LINE_PATTERN = re.compile(r'^Game (\d+):(.*)$')
+LINE_PATTERN = re.compile(r"^Game (\d+):(.*)$")
+
 
 def create_block_dict(grab: str) -> dict[str, int]:
     # First, pull out the count and color
-    count_colors = [x.strip().split(' ') for x in grab.split(',')]
+    count_colors = [x.strip().split(" ") for x in grab.split(",")]
     # Then make it in a dictionary, so we can handle this easier
     return {color: int(count) for count, color in count_colors}
 
+
 # Used for part 1, along with the static `LIMITS`
 def valid_input(color_counts: dict[str, int]) -> bool:
-    for color, count  in color_counts.items():
+    for color, count in color_counts.items():
         if count > LIMITS[color]:
             return False
     return True
 
+
 def parse_line(line: str, part_two: bool) -> int:
     match = LINE_PATTERN.match(line)
     if match is None:
-        print('Error, shouldn\'t happen')
+        print("Error, shouldn't happen")
         return 0
     num, remainder = match.group(1, 2)
     num = int(num)
     # Split into each observation of bag contents
-    individual_grabs = remainder.split(';')
+    individual_grabs = remainder.split(";")
 
     # Going to keep logic running to calculate both ways, then just return the result based on `part_two`
     min_counts = defaultdict(int)
@@ -49,44 +55,47 @@ def parse_line(line: str, part_two: bool) -> int:
         return math.prod(min_counts.values())
     else:
         return num if valid else 0
-    
+
+
 def solution(data: str, part_two: bool) -> int:
     total = 0
-    for line in data.split('\n'):
+    for line in data.split("\n"):
         total += parse_line(line, part_two)
     return total
+
 
 def main():
     parser = ArgumentParser()
     parser.add_argument(
         "-f",
-        '--filename',
+        "--filename",
         default=None,
-        help='Filename containing input data. If not provided, uses the example from the problem',
+        help="Filename containing input data. If not provided, uses the example from the problem",
     )
 
     parser.add_argument(
-        '--part-two',
+        "--part-two",
         default=False,
-        action='store_true',
-        help='To produce output for the part2 version of this problem',
+        action="store_true",
+        help="To produce output for the part2 version of this problem",
     )
 
     args = parser.parse_args()
 
-    data = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    data = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'''
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
 
     if args.filename:
         with open(args.filename) as f:
             data = f.read()
-    
+
     output = solution(data, args.part_two)
-    
-    print(f'Solution: {output}')
+
+    print(f"Solution: {output}")
+
 
 if __name__ == "__main__":
     main()
