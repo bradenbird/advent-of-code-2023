587979e56b1acf58078ede2048fb2e169ab7c3bc
Braden Bird <15621005+bradenbird@users.noreply.github.com>
2023-12-03 01:42:22 -0600

    Day 1, part 2 completed
    Went ahead refactored the original part 1 solution too.
    This way more logic is shared between the approaches and
    a simple bool parameter can choose part 1 or 2 functionality

--- day1/day1.py
+++ day1/day1.py
@@ -1,35 +1,52 @@
 from argparse import ArgumentParser
 
-# Could probably play with some more efficient checks than hashing and looking up in a set, but we can start with this
-def number_from_line(line) -> int:
-    DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+DIGIT_STRS = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    'zero': 0,
+}
+
+def get_left_number(line: str, digit_only: bool):
     length = len(line)
-    left_digit = 0
-    while left_digit < length:
-        if line[left_digit] in DIGITS:
-            break
-        left_digit += 1
+    start_index = 0
+    while start_index < length:
+        if line[start_index].isdigit():
+            return int(line[start_index])
+        if not digit_only:
+            for s, num in DIGIT_STRS.items():
+                if line[start_index:].startswith(s):
+                    return num
+        start_index += 1
+    # No matches, return 0
+    return 0
 
-    if left_digit == length:
-        return 0
+def get_right_number(line: str, digit_only: bool):
+    end_index = len(line) - 1
+    while end_index >= 0:
+        if line[end_index].isdigit():
+            return int(line[end_index])
+        if not digit_only:
+            for s, num in DIGIT_STRS.items():
+                if line[:end_index+1].endswith(s):
+                    return num
+        end_index -= 1
+    # No matches, return 0
+    return 0
 
-    right_digit = length - 1
-    while right_digit >= 0:
-        if line[right_digit] in DIGITS:
-            break
-        right_digit -= 1
-    
-    if right_digit < 0:
-        return 0
+def number_from_line(line: str, digit_only: bool) -> int:
+    return get_left_number(line, digit_only) * 10 + get_right_number(line, digit_only)
 
-    # Should have 2 valid digits now, convert from str to int
-    return int(f'{line[left_digit]}{line[right_digit]}')
-
-
-def solution(data: str) -> int:
+def solution(data: str, digit_only: bool) -> int:
     total = 0
     for line in data.split('\n'):
-        total += number_from_line(line)
+        total += number_from_line(line, digit_only)
     return total
 
 def main():
@@ -38,22 +55,37 @@ def main():
         "-f",
         '--filename',
         default=None,
-        help="Filename containing input data. If not provided, uses the example from the problem",
+        help='Filename containing input data. If not provided, uses the example from the problem',
+    )
+
+    parser.add_argument(
+        '--digit-only',
+        default=False,
+        action='store_true',
+        help='Use the part-1 line parsing on the input'
     )
 
     args = parser.parse_args()
 
-    data = '''1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
-'''
+# Part 1 example data, can use instead if running part 1
+#     data = '''1abc2
+# pqr3stu8vwx
+# a1b2c3d4e5f
+# treb7uchet'''
+
+    data = '''two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen'''
 
     if args.filename:
         with open(args.filename) as f:
             data = f.read()
     
-    output = solution(data)
+    output = solution(data, args.digit_only)
     
     print(f'Solution: {output}')
 
